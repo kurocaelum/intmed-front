cf579201d611cdc266bc28a09896693ba8337e06
Victor <victor_brandao@outlook.com>
2021-06-20 16:27:31 -0300

    refactor: route renaming

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent},
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
-  {path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
