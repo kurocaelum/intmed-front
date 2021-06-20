56bf8a50ea1098194011b8ba819a1c3d972a26fa
Victor <victor_brandao@outlook.com>
2021-06-20 17:08:50 -0300

    feat: auth WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {
+    path: '',
+    component: HomeComponent,
+    children: []
+  },
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
 ];
